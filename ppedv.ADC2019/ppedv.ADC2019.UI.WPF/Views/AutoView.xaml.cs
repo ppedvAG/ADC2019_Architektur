79379efd897c9674201f4eeb2e5af3317cc3d3ec
André <andrerrr@outlook.de>
2019-05-24 15:52:20 +0200

    View ist fein

---
+++ ppedv.ADC2019/ppedv.ADC2019.UI.WPF/Views/AutoView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ppedv.ADC2019.UI.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for AutoView.xaml
+    /// </summary>
+    public partial class AutoView : UserControl
+    {
+        public AutoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
