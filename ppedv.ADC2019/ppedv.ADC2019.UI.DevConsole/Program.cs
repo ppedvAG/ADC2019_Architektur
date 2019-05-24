6bf1669da9f999353d8774224b07a369c9519be1
André <andrerrr@outlook.de>
2019-05-24 12:00:44 +0200

    Beste UI = Console

---
+++ ppedv.ADC2019/ppedv.ADC2019.UI.DevConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using ppedv.ADC2019.Logic;
+using ppedv.ADC2019.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.ADC2019.UI.DevConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** ADC 2019 v01 ***");
+
+            var core = new Core();
+            foreach (var a in core.Repository.GetAll<Auto>())
+            {
+                Console.WriteLine($"Farbe: {a.Farbe}, Hersteller: {a.Hersteller}, Modell: {a.Modell}");
+            }
+
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+        }
+    }
+}
