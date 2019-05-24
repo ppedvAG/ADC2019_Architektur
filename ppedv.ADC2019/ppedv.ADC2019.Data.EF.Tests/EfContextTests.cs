2a7649e96d54af0143db4c4112e801083d4f3298
André <andrerrr@outlook.de>
2019-05-24 10:30:47 +0200

    Test für EF

---
+++ ppedv.ADC2019/ppedv.ADC2019.Data.EF.Tests/EfContextTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ppedv.ADC2019.Data.EF.Tests
+{
+    [TestClass]
+    public class EfContextTests
+    {
+        [TestMethod]
+        public void EfContext_can_create_database()
+        {
+            //Arrange
+            var con = new EfContext();
+            if (con.Database.Exists())
+                con.Database.Delete();
+
+            //Act
+            con.Database.Create();
+
+            //Assert
+            Assert.IsTrue(con.Database.Exists());
+        }
+    }
+}
---
+++ ppedv.ADC2019/ppedv.ADC2019.Data.EF.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ppedv.ADC2019.Data.EF.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ppedv.ADC2019.Data.EF.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("07c239f6-3bce-4c59-96c8-07c14c4ecc29")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
