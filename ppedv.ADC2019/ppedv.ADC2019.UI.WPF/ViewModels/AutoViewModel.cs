8e925a4cda788a27576708aafe025f3bc4aea2f5
André <andrerrr@outlook.de>
2019-05-24 16:24:37 +0200

    MVVM

--- ppedv.ADC2019/ppedv.ADC2019.UI.WPF/ViewModels/AutoViewModel.cs
+++ ppedv.ADC2019/ppedv.ADC2019.UI.WPF/ViewModels/AutoViewModel.cs
@@ -3,21 +3,41 @@ using ppedv.ADC2019.Model;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ppedv.ADC2019.UI.WPF.ViewModels
 {
-    public class AutoViewModel
+    public class AutoViewModel : INotifyPropertyChanged
     {
         public ObservableCollection<Auto> Autos { get; set; }
+
+        public Auto SelectedAuto
+        {
+            get => selectedAuto;
+            set
+            {
+                selectedAuto = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedAuto)));
+            }
+        }
+
+        public ICommand SaveCommand { get; set; }
+
         Core core = new Core();
+        private Auto selectedAuto;
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public AutoViewModel()
         {
             Autos = new ObservableCollection<Auto>(core.UnitOfWork.AutoRepository.GetAll());
+
+            SaveCommand = new RelayCommand(o => core.UnitOfWork.SaveAll());
         }
-        
+
     }
 }
