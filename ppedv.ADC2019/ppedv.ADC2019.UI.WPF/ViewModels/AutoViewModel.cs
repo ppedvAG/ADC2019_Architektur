2fa8a9f9a505cfee759aa231e72d63e8c1ecab80
André <andrerrr@outlook.de>
2019-05-24 16:04:52 +0200

    ViewModel in View

---
+++ ppedv.ADC2019/ppedv.ADC2019.UI.WPF/ViewModels/AutoViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ppedv.ADC2019.Logic;
+using ppedv.ADC2019.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.ADC2019.UI.WPF.ViewModels
+{
+    public class AutoViewModel
+    {
+        public ObservableCollection<Auto> Autos { get; set; }
+        Core core = new Core();
+
+        public AutoViewModel()
+        {
+            Autos = new ObservableCollection<Auto>(core.UnitOfWork.AutoRepository.GetAll());
+        }
+        
+    }
+}
