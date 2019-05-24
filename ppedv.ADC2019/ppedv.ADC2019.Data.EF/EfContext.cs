b935990c2887b05148d38c962c0f4e5efc6b721a
André <andrerrr@outlook.de>
2019-05-24 10:17:22 +0200

    Entity Framework konfiguriert

---
+++ ppedv.ADC2019/ppedv.ADC2019.Data.EF/EfContext.cs
@@ -0,0 +1,27 @@
+﻿using ppedv.ADC2019.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.ADC2019.Data.EF
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<Auto> Autos { get; set; }
+        public DbSet<Kunde> Kunden { get; set; }
+        public DbSet<Standort> Standorte { get; set; }
+        public DbSet<Vermietung> Vermietungen { get; set; }
+
+        public EfContext(string conString) : base(conString)
+        { }
+
+        public EfContext() : this($"Server=.\\SQLEXPRESS;Database=ADC2019_dev;Trusted_Connection=true")
+        //   public EfContext() : this($"Server=(localdb)\\mssqllocaldb;Database=ADC2019_dev;Trusted_Connection=true")
+        {
+
+        }
+    }
+}
