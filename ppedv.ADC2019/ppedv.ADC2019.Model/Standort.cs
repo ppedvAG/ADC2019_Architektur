65269817c427f5d79f3a3569fb903969c671b743
André <andrerrr@outlook.de>
2019-05-24 09:58:21 +0200

    Dominmodel erstellt

---
+++ ppedv.ADC2019/ppedv.ADC2019.Model/Standort.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ppedv.ADC2019.Model
+{
+    public class Standort : Entity
+    {
+        public string Name { get; set; }
+        public ICollection<Auto> Autos { get; set; } = new HashSet<Auto>();
+    }
+}
---
+++ ppedv.ADC2019/ppedv.ADC2019.Model/Vermietung.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ppedv.ADC2019.Model
+{
+    public class Vermietung : Entity
+    {
+        public DateTime Datum { get; set; }
+        public DateTime Ende { get; set; }
+        public virtual Standort StartStandort { get; set; }
+        public virtual Standort ZielStandort { get; set; }
+        public virtual Auto Auto { get; set; }
+        public virtual Kunde Kunde { get; set; }
+    }
+}
