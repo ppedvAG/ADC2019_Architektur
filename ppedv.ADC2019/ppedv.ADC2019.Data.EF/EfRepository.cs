e1219dddc155dd9c6282ae990596bc0b8fd5b388
André <andrerrr@outlook.de>
2019-05-24 11:46:13 +0200

    Ef Repo implementiert

---
+++ ppedv.ADC2019/ppedv.ADC2019.Data.EF/EfRepository.cs
@@ -0,0 +1,57 @@
+﻿using ppedv.ADC2019.Model;
+using ppedv.ADC2019.Model.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.ADC2019.Data.EF
+{
+    public class EfRepository : IRepository
+    {
+        EfContext con = new EfContext();
+
+        public void Add<T>(T entity) where T : Entity
+        {
+            //if (typeof(T) == typeof(Auto))
+            //    con.Autos.Add(entity as Auto);
+            con.Set<T>().Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : Entity
+        {
+            con.Set<T>().Remove(entity);
+
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : Entity
+        {
+            return con.Set<T>().ToList();
+        }
+
+        public T GetById<T>(int id) where T : Entity
+        {
+            return con.Set<T>().Find(id);
+        }
+
+        public IQueryable<T> Query<T>() where T : Entity
+        {
+            return con.Set<T>();
+        }
+
+        public void SaveAll()
+        {
+            con.SaveChanges();
+        }
+
+        public void Update<T>(T entity) where T : Entity
+        {
+            var loaded = GetById<T>(entity.Id);
+            if (loaded != null)
+            {
+                con.Entry(loaded).CurrentValues.SetValues(entity);
+            }
+        }
+    }
+}
